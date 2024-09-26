25d8615bf67d61bbd37ff98a0fec7b413171b241
ZOFUS <113346741+ZOFUS@users.noreply.github.com>
2024-09-27 01:12:50 +0300

    Add files via upload

---
+++ Zadanie_1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Zadanie_1
+{
+    partial class Form1
+    {
+        // Обязательная переменная конструктора
+        private System.ComponentModel.IContainer components = null;
+
+        // Освободить все используемые ресурсы
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        // Требуемый метод для поддержки конструктора — не изменяйте содержимое этого метода с помощью редактора кода.
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.number = new System.Windows.Forms.TextBox();
+            this.resultLabel1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.left = new System.Windows.Forms.TextBox();
+            this.right = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.resultLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(274, 62);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(226, 20);
+            this.number.TabIndex = 1;
+            // 
+            // resultLabel1
+            // 
+            this.resultLabel1.AutoSize = true;
+            this.resultLabel1.Location = new System.Drawing.Point(43, 182);
+            this.resultLabel1.Name = "resultLabel1";
+            this.resultLabel1.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.36F);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Преобразование числительных в цифры:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите числительные текстом (cent soixante-six)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.36F);
+            this.label4.Location = new System.Drawing.Point(12, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(292, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Перемещение слов в нужном диапозоне в конец:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Введите текст";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Начальный индекс";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 331);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Конечный индекс";
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(104, 275);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(190, 20);
+            this.inputText.TabIndex = 10;
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(122, 305);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(166, 20);
+            this.left.TabIndex = 11;
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(117, 331);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(171, 20);
+            this.right.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Перенести";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 405);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Результат:";
+            // 
+            // resultLabel2
+            // 
+            this.resultLabel2.AutoSize = true;
+            this.resultLabel2.Location = new System.Drawing.Point(43, 436);
+            this.resultLabel2.Name = "resultLabel2";
+            this.resultLabel2.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel2.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.resultLabel2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultLabel1);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Задание 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox number;
+        private System.Windows.Forms.Label resultLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox inputText; // Переименованное поле
+        private System.Windows.Forms.TextBox left;
+        private System.Windows.Forms.TextBox right;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label resultLabel2;
+    }
+}
---
+++ Zadanie_1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Zadanie_1
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<string, int> unitsMap = new Dictionary<string, int>
+        {
+            {"zéro", 0},
+            {"un", 1},
+            {"une", 1},
+            {"deux", 2},
+            {"trois", 3},
+            {"quatre", 4},
+            {"cinq", 5},
+            {"six", 6},
+            {"sept", 7},
+            {"huit", 8},
+            {"neuf", 9},
+            {"dix", 10},
+            {"onze", 11},
+            {"douze", 12},
+            {"treize", 13},
+            {"quatorze", 14},
+            {"quinze", 15},
+            {"seize", 16},
+            {"dix-sept", 17},
+            {"dix-huit", 18},
+            {"dix-neuf", 19}
+        };
+
+        private readonly Dictionary<string, int> tensMap = new Dictionary<string, int>
+        {
+            {"vingt", 20},
+            {"trente", 30},
+            {"quarante", 40},
+            {"cinquante", 50},
+            {"soixante", 60},
+            {"soixante-dix", 70},
+            {"quatre-vingt", 80},
+            {"quatre-vingts", 80},
+            {"quatre-vingt-dix", 90}
+        };
+
+        private readonly Dictionary<string, int> hundredsMap = new Dictionary<string, int>
+        {
+            {"cent", 100},
+            {"deux cents", 200},
+            {"trois cents", 300},
+            {"quatre cents", 400},
+            {"cinq cents", 500},
+            {"six cents", 600},
+            {"sept cents", 700},
+            {"huit cents", 800},
+            {"neuf cents", 900}
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = number.Text.ToLower().Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Вы ничего не ввели! Пожалуйста, введите числительное на французском.");
+                return;
+            }
+
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int result = 0;
+            bool isValidInput = true;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string writtenWord = words[i];
+
+                if (writtenWord == "et")
+                {
+                    continue;
+                }
+
+                if (i < words.Length - 1)
+                {
+                    string twoWords = writtenWord + " " + words[i + 1];
+                    if (hundredsMap.ContainsKey(twoWords))
+                    {
+                        result += hundredsMap[twoWords];
+                        i++;
+                        continue;
+                    }
+                }
+
+                if (hundredsMap.ContainsKey(writtenWord))
+                {
+                    result += hundredsMap[writtenWord];
+                    continue;
+                }
+
+                if (tensMap.ContainsKey(writtenWord))
+                {
+                    result += tensMap[writtenWord];
+                    continue;
+                }
+
+                if (writtenWord.Contains("-"))
+                {
+                    string[] hyphenParts = writtenWord.Split('-');
+
+                    int temp = 0;
+                    bool wordValid = true;
+                    int j = 0;
+                    while (j < hyphenParts.Length)
+                    {
+                        if (j < hyphenParts.Length - 1)
+                        {
+                            string twoParts = hyphenParts[j] + "-" + hyphenParts[j + 1];
+                            if (tensMap.ContainsKey(twoParts))
+                            {
+                                temp += tensMap[twoParts];
+                                j += 2;
+                                continue;
+                            }
+                        }
+
+                        string singlePart = hyphenParts[j];
+                        if (unitsMap.ContainsKey(singlePart))
+                        {
+                            temp += unitsMap[singlePart];
+                        }
+                        else if (tensMap.ContainsKey(singlePart))
+                        {
+                            temp += tensMap[singlePart];
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Введённое вами слово \"{singlePart}\" не является числительным. Пожалуйста, проверьте ввод.");
+                            isValidInput = false;
+                            wordValid = false;
+                            break;
+                        }
+                        j++;
+                    }
+
+                    if (wordValid)
+                    {
+                        result += temp;
+                        continue;
+                    }
+                }
+
+                if (unitsMap.ContainsKey(writtenWord))
+                {
+                    result += unitsMap[writtenWord];
+                    continue;
+                }
+
+                MessageBox.Show($"Введённое вами слово \"{writtenWord}\" не является числительным. Пожалуйста, проверьте ввод.");
+                isValidInput = false;
+                break;
+            }
+
+            if (isValidInput)
+            {
+                resultLabel1.Text = result.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputText.Text) ||
+                string.IsNullOrWhiteSpace(left.Text) ||
+                string.IsNullOrWhiteSpace(right.Text))
+            {
+                MessageBox.Show("Вы ввели не все значения. Пожалуйста, заполните все поля.");
+                return;
+            }
+
+            if (!(int.TryParse(left.Text, out int l) && int.TryParse(right.Text, out int r)))
+            {
+                MessageBox.Show("Один или несколько введенных вами индексов не являются числами. Пожалуйста, введите корректные числа.");
+                return;
+            }
+
+            string[] str = inputText.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (l < 1 || r < 1 || l > str.Length || r > str.Length || r < l)
+            {
+                MessageBox.Show("Вы ввели некорректные значения. Убедитесь, что индексы находятся в пределах текста.");
+                return;
+            }
+
+            if (l == r)
+            {
+                MessageBox.Show("Начальный и конечный индексы не могут быть одинаковыми.");
+                return;
+            }
+
+            r--;
+            l--;
+
+            string answer = "";
+
+            for (int i = 0; i < l; i++)
+            {
+                answer += str[i] + " ";
+            }
+
+            for (int i = r + 1; i < str.Length; i++)
+            {
+                answer += str[i] + " ";
+            }
+
+            for (int i = l; i <= r; i++)
+            {
+                answer += str[i] + " ";
+            }
+
+            resultLabel2.Text = answer.Trim();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
